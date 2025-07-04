b77708ee22525401b00a45e961a39ee418ece0a8
Fahril Hadi <fahrilhadi77@gmail.com>
2025-07-04 19:28:58 +0700

    Admin API - Login Repository and Service

---
+++ internal/core/service/auth_service.go
@@ -0,0 +1,73 @@
+package service
+
+import (
+	"context"
+	"time"
+
+	"github.com/fahrilhadi/blog-portal-news/config"
+	"github.com/fahrilhadi/blog-portal-news/internal/adapter/repository"
+	"github.com/fahrilhadi/blog-portal-news/internal/core/domain/entity"
+	"github.com/fahrilhadi/blog-portal-news/lib/auth"
+	"github.com/fahrilhadi/blog-portal-news/lib/conv"
+	"github.com/gofiber/fiber/v2/log"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var err error
+var code string
+
+type AuthService interface {
+	GetUserByEmail(ctx context.Context, req entity.LoginRequest) (*entity.AccessToken, error)
+}
+
+type authService struct {
+	authRepository repository.AuthRepository
+	cfg *config.Config
+	jwtToken auth.Jwt
+}
+
+// GetUserByEmail implements AuthService.
+func (a *authService) GetUserByEmail(ctx context.Context, req entity.LoginRequest) (*entity.AccessToken, error) {
+	result, err := a.authRepository.GetUserByEmail(ctx, req)
+	if err != nil {
+		code = "[SERVICE] GetUserByEmail - 1"
+		log.Errorw(code, err)
+		return nil, err
+	}
+
+	if checkPass := conv.CheckPasswordHash(req.Password, result.Password); !checkPass {
+		code = "[SERVICE] GetUserByEmail - 2"
+		log.Errorw(code, "Invalid password")
+		return nil, err
+	}
+
+	jwtData := entity.JwtData{
+		UserID: float64(result.ID),
+		RegisteredClaims: jwt.RegisteredClaims{
+			NotBefore: jwt.NewNumericDate(time.Now().Add(time.Hour * 2)),
+			ID: string(result.ID),
+		},
+	}
+
+	accessToken, expiresAt, err := a.jwtToken.GenerateToken(&jwtData)
+	if err != nil {
+		code = "[SERVICE] GetUserByEmail - 3"
+		log.Errorw(code, err)
+		return nil, err
+	}
+
+	resp := entity.AccessToken{
+		AccessToken: accessToken,
+		ExpiresAt: expiresAt,
+	}
+
+	return &resp, nil
+}
+
+func NewAuthService(authRepository repository.AuthRepository, cfg *config.Config, jwtToken auth.Jwt) AuthService {
+	return &authService{
+		authRepository: authRepository,
+		cfg: cfg,
+		jwtToken: jwtToken,
+	}
+}
